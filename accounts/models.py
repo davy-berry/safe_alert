8b02725e468b403570657f79be275a6962e5222d
davy-berry <davy.berry@yahoo.co.uk>
2026-09-11 00:14:25 +0100

    Add UserProfile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import user
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    COMMUNITY_USER = 'community_user'
+    COMMUNITY_ADMIN = 'community_admin'
+
+    ROLE_CHOICES = [
+        (COMMUNITY_USER, 'Community User'),
+        (COMMUNITY_ADMIN, 'Community Admin'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=COMMUNITY_USER)
+
+    def __str__(self): 
+        return f"{self.user.username} - {self.get_role_display()}"
