27bf61950bf63b27c39ab002290b6c766abfa2be
davy-berry <davy.berry@yahoo.co.uk>
2026-09-13 03:47:05 +0100

    Add user registration functionality and templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import RegistrationForm
+from .models import UserProfile
+
+
+def register(request):
+
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+
+            user = form.save()
+
+            UserProfile.objects.create(
+                user=user,
+                role="community_member"
+            )
+
+            login(request, user)
+
+            return redirect("my_reports")
+
+    else:
+        form = RegistrationForm()
+
+    return render(
+        request,
+        "registration/register.html",
+        {
+            "form": form,
+        }
+    )
