f1b2fa3c52d79197cb58cf6e12491ceac930e6a0
davy-berry <davy.berry@yahoo.co.uk>
2026-09-14 09:05:35 +0100

    Add accounts URL configuration for user management

--- main/urls.py
+++ main/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reports/', include('reports.urls')),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
