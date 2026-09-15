e09009500df14f0f911540b9a543215db5d83621
davy-berry <davy.berry@yahoo.co.uk>
2026-09-15 22:45:30 +0100

    Adding styling and boostarps layout to login and register page

--- reports/views.py
+++ reports/views.py
@@ -85,10 +85,7 @@ def edit_report(request, report_id):
     return render(
         request,
         "reports/edit_report.html",
-        {
-            "form": form,
-            "report": report,
-        }
+        {"form": form, "report": report, "stadia_api_key": settings.STADIA_API_KEY,}
     )
 
 @login_required
