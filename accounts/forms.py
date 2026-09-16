9c27d5d05752bb7c6d8bb29c4f19a5c72c81f409
davy-berry <davy.berry@yahoo.co.uk>
2026-09-16 11:15:59 +0100

    Setting up new registration with AllAuth

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,53 +5,6 @@ from allauth.account.forms import SignupForm
 
 from .models import UserProfile
 
-
-class RegistrationForm(forms.ModelForm):
-
-    password = forms.CharField(
-        widget=forms.PasswordInput
-    )
-
-    password_confirm = forms.CharField(
-        widget=forms.PasswordInput
-    )
-
-    class Meta:
-        model = User
-
-        fields = [
-            "username",
-            "email",
-            "password",
-            "password_confirm",
-        ]
-
-    def clean(self):
-        cleaned_data = super().clean()
-
-        password = cleaned_data.get("password")
-        password_confirm = cleaned_data.get("password_confirm")
-
-        if password and password_confirm:
-            if password != password_confirm:
-                raise forms.ValidationError(
-                    "The passwords do not match."
-                )
-
-        return cleaned_data
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-
-        user.set_password(
-            self.cleaned_data["password"]
-        )
-
-        if commit:
-            user.save()
-
-        return user
-
 class SafeAlertSignupForm(SignupForm):
 
     def save(self, request):
