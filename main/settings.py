17530992a5244ef1fda751fca8fec1adc14084a1
davy-berry <davy.berry@yahoo.co.uk>
2026-09-18 13:13:51 +0100

    Custom 404 error page

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=^t09j974w!!9w$#$)f4(ox-%w_yi%$ubet-xa)*0_=us&%*em'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['.herokuapp.com','localhost', '127.0.0.1']
 
