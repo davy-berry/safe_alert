5131fa716fdb634823afefd38d4300989d033e7c
davy-berry <davy.berry@yahoo.co.uk>
2026-09-22 15:30:33 +0100

    Remove commented-out code for test database configuration in settings.py

--- main/settings.py
+++ main/settings.py
@@ -90,8 +90,8 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-if 'test' in sys.argv:
-    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+#if 'test' in sys.argv:
+#   DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
