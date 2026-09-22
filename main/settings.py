26998689c8f1ad4c3b94683bb80c7fd75d8f30fc
davy-berry <davy.berry@yahoo.co.uk>
2026-09-22 15:39:16 +0100

    Update DEBUG setting to use environment variable and adjust database engine for test mode

--- main/settings.py
+++ main/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['.herokuapp.com','localhost', '127.0.0.1']
 
@@ -90,8 +90,8 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-#if 'test' in sys.argv:
-#   DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
